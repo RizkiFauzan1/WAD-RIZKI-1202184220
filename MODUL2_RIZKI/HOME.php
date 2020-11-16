d611b840928f4b228bbfbe748e2758eede121119
RizkiFauzan1 <ikyfauzan16@gmail.com>
2020-11-16 18:09:57 +0700

    izin mengumpulkan modul 2 nya ka

---
+++ MODUL2_RIZKI/HOME.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>YUK LIBURAN.COM</title>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="HOME.php">HOME<span class="sr-only">(current)</span></a>
+            </li>
+            
+            <li class="nav-item">
+                  <a class="nav-link" href="Booking.php">Booking</a>
+            </li>
+          </ul>
+        </div>
+    </div>
+        </div>
+    
+      </nav>
+  
+    <div class="text center">
+      <div class="text-primary">
+      <p><h2 align="center " > EAD HOTEL</h2>
+      <h3 align="center">Welcome To 5 Star Hotel</h3>
+    </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+          <div class="col text-center">
+      <div class="card-deck">
+        <div class="card">
+          <img src="1.jpg" class="card-img-top" alt="">
+          <div class="card-body">
+            <div class="text-center">
+              <div class="text-primary">
+            <h5 class="card-title" align="center">Standard</h5>
+            <h5 class="card-title" align="center">$ 90/day</h5>
+            <br>
+              </div>
+            </div>
+            
+            <p align="center">Facilities</p>
+            <hr>
+            <p align="center">1 Single Bed</p>
+            <hr>
+            <p align="center"> 1 Bathroom</p>
+            <hr>
+            <center>
+            <a href="#"  class="btn btn-primary">Book Now</a>
+            </center>
+          </div>
+        </div>
+        
+        <div class="card">
+          <img src="2.jpg" class="card-img-top" alt="">
+          <div class="card-body">
+            <div class="text center">
+              <div class="text-primary">
+            <h5 class="card-title" align="center">Superior</h5>
+            <h5 class="card-title" align="center">$150/day</h5>
+            <br>
+          </div>
+        </div>
+            
+            <p align="center">Facilities</p>
+            <hr>
+            <p align="center">1 Double Bed</p>
+            <hr>
+            <p align="center"> 1 Television and Wi-Fi</p>
+            <hr>
+            <p align="center"> 1 Bathroom with hot water</p>
+            <hr>
+
+            <center>
+            <a href="#"  class="btn btn-primary">Book Now</a>
+            </center>
+          </div>
+        </div>
+        <div class="card">
+          <img src="3.jpg" class="card-img-top" >
+          <div class="card-body">
+            <div class="text center">
+              <div class="text-primary">
+            <h5 class="card-title"align="center">Luxury</h5>
+            <h5 class="card-title"align="center">$200/day</h5>
+            <br>
+              </div>
+            </div>
+            
+            <p align="center">Facilities</p>
+            <hr>
+            <p align="center">2 Double Bed</p>
+            <hr>
+            <p align="center">2 Bathroom with hot water</p>
+            <hr>
+            <p align="center"> 1 Kitchen</p>
+            <hr>
+            <p align="center"> 1 Television and Wi-Fi</p>
+            <hr>
+            <p align="center"> 1 Workroom</p>
+            <hr>
+  
+            <center>
+            <a href="#"  class="btn btn-primary " btn center>Book Now</a>
+        </center>
+          
+          </div>
+        </div>
+      </div>
+    </body>
+</html>
